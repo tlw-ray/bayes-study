f11ad60c7ab24865b48eadeca7041d0f7715dc95
tlw_ray <tlw_ray@163.com>
2020-10-11 10:15:20 +0800

    apriori

--- src/main/java/com/tlw/ml/subterm/apriori/Apriori.java
+++ src/main/java/com/tlw/ml/subterm/apriori/Apriori.java
@@ -1,11 +1,13 @@
 package com.tlw.ml.subterm.apriori;
 
 import org.apache.commons.collections4.set.ListOrderedSet;
+import org.apache.commons.math3.util.Combinations;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author 唐力伟
@@ -19,291 +21,81 @@ import java.util.Map;
  */
 public class Apriori {
 
-    boolean endTag = false;
-    // k-1频繁集的记数表
-    Map<Integer, Integer> dCountMap = new HashMap();
-    // k频繁集的记数表
-    Map<Integer, Integer> dkCountMap = new HashMap();
-    // 数据记录表
-    List<ListOrderedSet<Integer>> record = new ArrayList();
-    // 最小支持度
-    final double MIN_SUPPORT = 0.2;
-    // 最小置信度
-    final double MIN_CONF = 0.8;
-    // 置信度记录表
-    List<Double> confCount = new ArrayList();
-    // 满足支持度的集合
-    List<ListOrderedSet<Integer>> confItemSet = new ArrayList();
+    ListOrderedSet<ListOrderedSet<Integer>> input;
+    ListOrderedSet<Map<ListOrderedSet<Integer>, Integer>> output = new ListOrderedSet();
 
-    public Apriori(List<ListOrderedSet<Integer>> record){
-        this.record = record;
-        List<ListOrderedSet<Integer>> cItemSet = findFirstCandidate();// 获取第一次的备选集
-        List<ListOrderedSet<Integer>> lItemSet = getSupportedItemSet(cItemSet);// 获取备选集cItemSet满足支持的集合
+    int level = 1;
 
-        while (endTag != true) {// 只要能继续挖掘
-            List<ListOrderedSet<Integer>> ckItemSet = getNextCandidate(lItemSet);// 获取第下一次的备选集
-            List<ListOrderedSet<Integer>> lkItemSet = getSupportedItemSet(ckItemSet);// 获取备选集cItemSet满足支持的集合
-            getConfidencedItemSet(lkItemSet, lItemSet, dkCountMap, dCountMap);// 获取备选集cItemSet满足置信度的集合
-            if (confItemSet.size() != 0) {// 满足置信度的集合不为空
-                printConfItemSet(confItemSet);// 打印满足置信度的集合
+    public void process(){
+        ListOrderedSet<Integer> allTerms = getAllTerms();
+        Map<ListOrderedSet<Integer>, Integer> termsCountMap = countTermSet(allTerms, level++);
+        output.add(termsCountMap);
+        do{
+            allTerms = getAllTerm(termsCountMap);
+            Map<ListOrderedSet<Integer>, Integer> newTermsCountMap = countTermSet(allTerms, level++);
+            if(newTermsCountMap.size() > 0){
+                output.add(newTermsCountMap);
+            }else{
+                break;
             }
-            confItemSet.clear();// 清空置信度的集合
-            cItemSet = ckItemSet;// 保存数据，为下次循环迭代准备
-            lItemSet = lkItemSet;
-            dCountMap.clear();
-            dCountMap.putAll(dkCountMap);
-        }
+        }while(level < allTerms.size());
+        printOutput();
     }
 
-    /**
-     * @param confItemSet2
-     * 输出满足条件的频繁集
-     */
-    private void printConfItemSet(List<ListOrderedSet<Integer>> confItemSet2) {
-        System.out.print("*********频繁模式挖掘结果***********\n");
-        for (int i = 0; i < confItemSet2.size(); i++) {
-            int j = 0;
-            for (j = 0; j < confItemSet2.get(i).size() - 3; j++) {
-                System.out.print(confItemSet2.get(i).get(j) + " ");
-            }
-            System.out.print("-->");
-            System.out.print(confItemSet2.get(i).get(j++));
-            System.out.print("相对支持度：" + confItemSet2.get(i).get(j++));
-            System.out.print("置信度：" + confItemSet2.get(i).get(j++) + "\n");
-        }
-
+    // 首次取得所有因此的集合
+    private ListOrderedSet<Integer> getAllTerms(){
+        Set<Integer> terms = input.stream().flatMap(e -> e.stream()).collect(Collectors.toSet());
+        return ListOrderedSet.listOrderedSet(terms);
     }
 
-    /**
-     * @param lkItemSet
-     * @param lItemSet
-     * @param dkCountMap2
-     * @param dCountMap2
-     * 根据lkItemSet，lItemSet，dkCountMap2，dCountMap2求出满足置信度的集合
-     */
-    private List<ListOrderedSet<Integer>> getConfidencedItemSet(
-            List<ListOrderedSet<Integer>> lkItemSet, List<ListOrderedSet<Integer>> lItemSet,
-            Map<Integer, Integer> dkCountMap2, Map<Integer, Integer> dCountMap2) {
-        for (int i = 0; i < lkItemSet.size(); i++) {
-            getConfItem(lkItemSet.get(i), lItemSet, dkCountMap2.get(i),
-                    dCountMap2);
-
+    // 取得所有因子的集合
+    private ListOrderedSet<Integer> getAllTerm(Map<ListOrderedSet<Integer>, Integer> countMap){
+        ListOrderedSet<Integer> result = new ListOrderedSet();
+        for(ListOrderedSet<Integer> keySet:countMap.keySet()){
+            result.addAll(keySet);
         }
-        return null;
+        return result;
     }
 
-    /**
-     * @param list
-     * @param lItemSet
-     * @param count
-     * @param dCountMap2
-     * 检验集合list是否满足最低置信度要求
-     * 若满足则在全局变量confItemSet添加list
-     * 如不满足则返回null
-     */
-    private List<Integer> getConfItem(ListOrderedSet<Integer> list,
-                                            List<ListOrderedSet<Integer>> lItemSet, Integer count,
-                                            Map<Integer, Integer> dCountMap2) {
-        for (int i = 0; i < list.size(); i++) {
-            ListOrderedSet<Integer> testList = new ListOrderedSet();
-            for (int j = 0; j < list.size(); j++) {
-                if (i != j) {
-                    testList.add(list.get(j));
-                }
+    //为因子组合计数
+    private Map<ListOrderedSet<Integer>, Integer> countTermSet(ListOrderedSet<Integer> terms, int setSize){
+        Map<ListOrderedSet<Integer>, Integer> termSetCountMap = new HashMap();
+        Combinations combinations = new Combinations(terms.size(), setSize);
+        combinations.iterator().forEachRemaining(e -> {
+            int count = input.stream().map(i -> containAll(i, e)?1:0).reduce(0, Integer::sum);
+            ListOrderedSet<Integer> combination = new ListOrderedSet();
+            for(Integer v:e){
+                combination.add(v);
             }
-            int index = findConf(testList, lItemSet);//查找testList中的内容在lItemSet的位置
-            Double conf = count * 1.0 / dCountMap2.get(index);
-            if (conf > MIN_CONF) {//满足置信度要求
-                testList.add(list.get(i));
-                Double relativeSupport = count * 1.0 / (record.size() - 1);
-                testList.add(relativeSupport);
-                testList.add(conf);
-                confItemSet.add(testList);//添加到满足置信度的集合中
+            termSetCountMap.put(combination, count);
+        });
+
+        // 移除出现不足一次的
+        Set<ListOrderedSet<Integer>> removeSet = new HashSet();
+        for(Map.Entry<ListOrderedSet<Integer>, Integer> entry:termSetCountMap.entrySet()){
+            if(entry.getValue() < 2){
+                removeSet.add(entry.getKey());
             }
         }
-        return null;
+        termSetCountMap.keySet().removeAll(removeSet);
+        return termSetCountMap;
     }
 
-    /**
-     * @param testList
-     * @param lItemSet
-     * 查找testList中的内容在lItemSet的位置
-     */
-    private int findConf(ListOrderedSet<Integer> testList,
-                                List<ListOrderedSet<Integer>> lItemSet) {
-        for (int i = 0; i < lItemSet.size(); i++) {
-            boolean notHaveTag = false;
-            for (int j = 0; j < testList.size(); j++) {
-                if (lItemSet.get(i).contains(testList.get(j))) {
-                    notHaveTag = true;
-                    break;
-                }
-            }
-            if (notHaveTag == false) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * @param cItemSet
-     * 求出cItemSet中满足最低支持度集合
-     */
-    private List<ListOrderedSet<Integer>> getSupportedItemSet(
-            List<ListOrderedSet<Integer>> cItemSet) {
-        boolean end = true;
-        List<ListOrderedSet<Integer>> supportedItemSet = new ArrayList();
-        int k = 0;
-        for (int i = 0; i < cItemSet.size(); i++) {
-            int count = countFrequent(cItemSet.get(i));//统计记录数
-            if (count >= MIN_SUPPORT * (record.size() - 1)) {// count值大于支持度与记录数的乘积，即满足支持度要求
-                if (cItemSet.get(0).size() == 1)
-                    dCountMap.put(k++, count);
-                else
-                    dkCountMap.put(k++, count);
-                supportedItemSet.add(cItemSet.get(i));
-                end = false;
-            }
-        }
-        endTag = end;
-        return supportedItemSet;
-    }
-
-    /**
-     * @param list
-     * 统计数据库记录record中出现list中的集合的个数
-     */
-    private int countFrequent(ListOrderedSet<Integer> list) {
-        int count = 0;
-        for (int i = 1; i < record.size(); i++) {
-            boolean notHavaThisList = false;
-            for (int k = 0; k < list.size(); k++) {
-                boolean thisRecordHave = false;
-                for (int j = 1; j < record.get(i).size(); j++) {
-                    if (list.get(k).equals(record.get(i).get(j)))
-                        thisRecordHave = true;
-                }
-                if (!thisRecordHave) {// 扫描一遍记录表的一行，发现list.get(i)不在记录表的第j行中，即list不可能在j行中
-                    notHavaThisList = true;
-                    break;
-                }
-            }
-            if (notHavaThisList == false)
-                count++;
-        }
-        return count;
-    }
-
-    /**
-     * @param cItemSet
-     * @return nextItemSet
-     * 根据cItemSet求出下一级的备选集合组，求出的备选集合组中的每个集合的元素的个数比cItemSet中的集合的元素大1
-     */
-    private List<ListOrderedSet<Integer>> getNextCandidate(
-            List<ListOrderedSet<Integer>> cItemSet) {
-        List<ListOrderedSet<Integer>> nextItemSet = new ArrayList();
-        for (int i = 0; i < cItemSet.size(); i++) {
-            ListOrderedSet<Integer> tempList = new ListOrderedSet();
-            for (int k = 0; k < cItemSet.get(i).size(); k++) {
-                tempList.add(cItemSet.get(i).get(k));
-            }
-            for (int h = i + 1; h < cItemSet.size(); h++) {
-                for (int j = 0; j < cItemSet.get(h).size(); j++) {
-                    tempList.add(cItemSet.get(h).get(j));
-                    if (isSubsetInC(tempList, cItemSet)) {// tempList的子集全部在cItemSet中
-                        ListOrderedSet<Integer> copyValueHelpList = new ListOrderedSet();
-                        for (int p = 0; p < tempList.size(); p++)
-                            copyValueHelpList.add(tempList.get(p));
-                        if (isHave(copyValueHelpList, nextItemSet))//nextItemSet还没有copyValueHelpList这个集合
-                            nextItemSet.add(copyValueHelpList);
-                    }
-                    tempList.remove(tempList.size() - 1);
-                }
-            }
-        }
-
-        return nextItemSet;
-    }
-
-    /**
-     * @param copyValueHelpList
-     * @param nextItemSet
-     * @return boolean
-     * 检验nextItemSet中是否包含copyValueHelpList
-     */
-    private boolean isHave(ListOrderedSet<Integer> copyValueHelpList,
-                                  List<ListOrderedSet<Integer>> nextItemSet) {
-        for (int i = 0; i < nextItemSet.size(); i++) {
-            if (copyValueHelpList.equals(nextItemSet.get(i))) {
+    private boolean containAll(ListOrderedSet<Integer> items, int[] terms){
+        for(int value: terms){
+            if(!items.contains(value)){
                 return false;
             }
         }
         return true;
     }
 
-    /**
-     * @param tempList
-     * @param cItemSet
-     * @return
-     *检验 tempList是不是cItemSet的子集
-     */
-    private boolean isSubsetInC(ListOrderedSet<Integer> tempList,
-                                       List<ListOrderedSet<Integer>> cItemSet) {
-        boolean haveTag = false;
-        for (int i = 0; i < tempList.size(); i++) {// k集合tempList的子集是否都在k-1级频繁级中
-            ListOrderedSet<Integer> testList = new ListOrderedSet();
-            for (int j = 0; j < tempList.size(); j++) {
-                if (i != j) {
-                    testList.add(tempList.get(j));
-                }
-            }
-            for (int k = 0; k < cItemSet.size(); k++) {
-                if (testList.equals(cItemSet.get(k))) {// 子集存在于k-1频繁集中
-                    haveTag = true;
-                    break;
-                }
-            }
-            if (haveTag == false) {// 其中一个子集不在k-1频繁集中
-                return false;
+    public void printOutput(){
+        for(Map<ListOrderedSet<Integer>, Integer> map:output){
+            System.out.println("--------");
+            for(Map.Entry<ListOrderedSet<Integer>, Integer> termGroups: map.entrySet()){
+                System.out.println("\t" + termGroups.getKey() + "\t" + termGroups.getValue());
             }
         }
-
-        return haveTag;
-    }
-
-    /**
-     *根据数据库记录求出第一级备选集
-     */
-    private List<ListOrderedSet<Integer>> findFirstCandidate() {
-        List<ListOrderedSet<Integer>> tableList = new ArrayList();
-        ListOrderedSet<Integer> lineList = new ListOrderedSet();
-
-        int size = 0;
-        for (int i = 1; i < record.size(); i++) {
-            for (int j = 1; j < record.get(i).size(); j++) {
-                if (lineList.isEmpty()) {
-                    lineList.add(record.get(i).get(j));
-                } else {
-                    boolean haveThisItem = false;
-                    size = lineList.size();
-                    for (int k = 0; k < size; k++) {
-                        if (lineList.get(k).equals(record.get(i).get(j))) {
-                            haveThisItem = true;
-                            break;
-                        }
-                    }
-                    if (haveThisItem == false) {
-                        lineList.add(record.get(i).get(j));
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < lineList.size(); i++) {
-            ListOrderedSet<Integer> helpList = new ListOrderedSet();
-            helpList.add(lineList.get(i));
-            tableList.add(helpList);
-        }
-        return tableList;
     }
 }
--- src/test/java/com/tlw/ml/subterm/apriori/TestApriori.java
+++ src/test/java/com/tlw/ml/subterm/apriori/TestApriori.java
@@ -1,28 +1,59 @@
 package com.tlw.ml.subterm.apriori;
 
-import org.apache.commons.collections.set.ListOrderedSet;
+import org.apache.commons.collections4.set.ListOrderedSet;
+import org.apache.commons.math3.util.Combinations;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TestApriori {
     @Test
     public void testApriori(){
-        Set<Integer> set1 = createSet(new int[]{1, 3, 4});
-        Set<Integer> set2 = createSet(new int[]{2, 3, 5});
-        Set<Integer> set3 = createSet(new int[]{1, 2, 3, 5});
-        Set<Integer> set4 = createSet(new int[]{2, 4});
-        Set[] sets = new Set[]{set1, set2, set3, set4};
+        ListOrderedSet<Integer> set1 = createSet(new Integer[]{0, 2, 3});
+        ListOrderedSet<Integer> set2 = createSet(new Integer[]{1, 2, 4});
+        ListOrderedSet<Integer> set3 = createSet(new Integer[]{0, 1, 2, 4});
+        ListOrderedSet<Integer> set4 = createSet(new Integer[]{1, 4});
+        ListOrderedSet<ListOrderedSet<Integer>> input = new ListOrderedSet();
+        input.add(set1);
+        input.add(set2);
+        input.add(set3);
+        input.add(set4);
         Apriori apriori = new Apriori();
-        apriori.input = sets;
+        apriori.input = input;
+        apriori.process();
 
 
     }
 
-    public Set<Integer> createSet(int[] values){
-        return new HashSet(Arrays.asList(values));
+    public ListOrderedSet<Integer> createSet(Integer[] values){
+        Set<Integer> treeSet = new HashSet();
+        treeSet.addAll(Arrays.asList(values));
+        return ListOrderedSet.listOrderedSet(treeSet);
+    }
+
+    @Test
+    public void testCombination(){
+        Combinations combinations = new Combinations(3, 1);
+        combinations.iterator().forEachRemaining(e -> System.out.println(Arrays.toString(e)));
+    }
+
+    @Test
+    public void testMapReduce(){
+        int[] ints = new int[]{1,2,3,4};
+        int count = Arrays.stream(ints).map(i -> i % 2).reduce(0, Integer::sum);
+        System.out.println(count);
+    }
+
+    @Test
+    public void testMapReduce2(){
+        int[] ints1 = new int[]{1,2,3,4};
+        int[] ints2 = new int[]{1,2,5};
+        int[][] ints = new int[][]{ints1, ints2};
+//        Arrays.stream(ints1).boxed().collect(Collectors.toSet());
+        int count = Arrays.stream(ints).flatMapToInt(x -> Arrays.stream(x)).boxed().collect(Collectors.toSet()).size();
+        System.out.println(count);
     }
 }
